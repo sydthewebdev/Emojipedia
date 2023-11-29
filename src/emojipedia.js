cc8a79852bff689df205ae401c0f6b0599f93d11
Sydney Alexis <133697035+sydthewebdev@users.noreply.github.com>
2023-11-29 16:02:40 -0400

    Initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,46 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💅",
+    name: "Painting Nails",
+    meaning:
+      "Woman painting her fingernails is preparing for a special occasion or is dressing up to go out. Can stand for beauty in general, femininity or a manicure appointment. Represents casualness and carefreeness."
+  },
+  {
+    id: 2,
+    emoji: "💞",
+    name: "Revolving Hearts",
+    meaning:
+      "Two hearts circling around each other represent the love between two persons. Someone wants to win your heart."
+  },
+  {
+    id: 3,
+    emoji: "✨",
+    name: "Sparkles",
+    meaning:
+      "Three decoratively sparkling stars. For example, can mean: I polished my car or my apartment to mirror finish."
+  },
+  {
+    id: 4,
+    emoji: "💃",
+    name: "Salsa Dancing Lady",
+    meaning:
+      "“I want to party” or “I'm so excited!” The dancing woman in the red dress is also often associated with salsa. In general stands for fun and joy."
+  },
+  {
+    id: 5,
+    emoji: "🛩",
+    name: "Plane",
+    meaning:
+      "You want to get away from here and would like to go on holiday by plane. Or the mobile phone is in flight mode. Flying is one of the safest means of transportation, only the elevator is safer."
+  },
+  {
+    id: 6,
+    emoji: "🔮",
+    name: "Crystal Ball",
+    meaning:
+      "What will happen? The crystal ball symbolizes supernatural perceptions. Is used to look ahead and predict the future."
+  }
+];
+
+export default emojipedia;
